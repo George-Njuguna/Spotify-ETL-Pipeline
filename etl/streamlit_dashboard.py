fc9dc88d24e3862163d7e396af087bb4f39fa3c9
George-Njuguna <mailtomegeorge0@gmail.com>
2025-12-04 16:26:56 +0300

    added kpi and line graph

--- etl/streamlit_dashboard.py
+++ etl/streamlit_dashboard.py
@@ -43,7 +43,7 @@ def get_connection():
         dbname=os.getenv('database'),
         user=os.getenv('postgre_account'),
         password=os.getenv('postgre_password'),
-        host=os.getenv('host'),
+        host="localhost",
         port=os.getenv('port')
     )
 
@@ -55,10 +55,45 @@ def load_cached_data(table):
     return import_data(table, conn)
 
 # Tables
-playlists_df =load_cached_data("playlist_data")
-followed_artists_df =load_cached_data("followed_artists_data")
-saved_albums_df =load_cached_data("saved_albums_data")
-recently_played_df =load_cached_data("recently_played_data")
-top_artists_df =load_cached_data("top_artists_data")
-top_tracks_df =load_cached_data("top_tracks_data")
+playlists_df = load_cached_data("playlists")
+followed_artists_df = load_cached_data("followed_artists")
+saved_albums_df = load_cached_data("saved_albums")
+recently_played_df = load_cached_data("recently_played_tracks")
+top_artists_df = load_cached_data("top_artists")
+top_tracks_df = load_cached_data("top_tracks")
+saved_tracks_df = load_cached_data("saved_tracks")
 
+ # Setting Header
+st.title("SPOTIFY WRAPPED DASHBOARD")
+
+ # setting tabs
+T1,T2,T3,T4,T5,T6 = st.tabs(["Overview", "Listening Stats", "Playlists", "Albums", "Saved Songs", "Artists"])
+
+with T1:
+    k1, k2, k3, k4 = st.columns([1,1,1,1])
+    k1.metric("Saved Tracks", f"{(saved_tracks_df.shape)[0]:,}")
+    k2.metric("Saved Albums", f"{(saved_albums_df.shape)[0]}")
+    k3.metric("Playlists", f"{(playlists_df.shape)[0]}")
+    k4.metric("Followed Artists", f"{(followed_artists_df.shape)[0]}")
+
+    col1, col2 = st.columns([3, 1])
+
+     # line graph 
+    listening_hourly = recently_played_df.resample("d", on="played_at")["duration"].sum().rolling(3).mean().rename("All").reset_index()
+    df_melted = listening_hourly.rename(columns={"All": "Total Listening Time (Second)", "played_at" : "Hour of Day"})
+
+    with col1:
+            with st.container(border=True):
+                fig = px.line(
+                    df_melted,
+                    x="Hour of Day",
+                    y="Total Listening Time (Second)",
+                    line_shape="spline",
+                    title="Hourly Listening Trend"
+                )
+
+                fig.update_traces(mode="lines+markers")
+                st.plotly_chart(fig, width='stretch', theme="streamlit")
+
+    
+    
