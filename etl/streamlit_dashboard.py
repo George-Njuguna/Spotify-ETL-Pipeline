682441a5404145c63235b93f20b15b964e558d57
George-Njuguna <mailtomegeorge0@gmail.com>
2026-01-09 20:18:31 +0300

    .

--- etl/streamlit_dashboard.py
+++ etl/streamlit_dashboard.py
@@ -1,14 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import seaborn as sns
 import matplotlib.pyplot as plt
 import plotly.express as px
 import psycopg2
 import os
 from dotenv import load_dotenv
-import datetime
-from scipy.stats import gaussian_kde
 import plotly.graph_objects as go
 from Functions import import_data
 
